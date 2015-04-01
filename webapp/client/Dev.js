ce8d74106c1cc043dd2d59644d703ddd3b2d23f3
Woong Park <feelblu@gmail.com>
2015-04-01 17:22:34 +0900

    Closes #29 json으로 정된 맵을 읽어서 canvas 화면에 표시하였음

--- webapp/client/Dev.js
+++ webapp/client/Dev.js
@@ -116,6 +116,8 @@ function init() {
 	Blockly.Xml.domToWorkspace(Blockly.mainWorkspace,
 	    $(startblock).get(0));
 
+  drawMap();
+
 }
 
 
@@ -137,5 +139,95 @@ function runCode() {
     eval(code);
   } catch (e) {
     alert(e);
+  }
+}
+
+function drawMap() {
+  var map = {
+    type: "maze",
+    imgs: {
+      background: "",
+      character: "/img/map/mouse.png",
+      goal: "/img/map/cheese.png",
+      objects: ["/img/map/box.png"]
+    },
+    coords: {
+      character: "3,4",
+      goal: "5,4",
+      roads: ["4,4"]
+    }
+  };
+  extractMap(map);
+
+  var deferreds = [
+    loadImg(map.imgs.character),
+    loadImg(map.imgs.goal)
+  ];
+  for(var i = 0; i < map.imgs.objects.length; i++) {
+    deferreds.push( loadImg(map.imgs.objects[i]) );
+  }
+
+  var $when = $.when.apply($, deferreds);
+  $when.done(function () {
+    var stage = new createjs.Stage("display");
+    var character = new createjs.Bitmap(map.imgs.character);
+    var goal = new createjs.Bitmap(map.imgs.goal);
+    character.x = map.coords.character.x * 50;
+    character.y = map.coords.character.y * 50;
+    stage.addChild(character);
+    goal.x = map.coords.goal.x * 50;
+    goal.y = map.coords.goal.y * 50;
+    stage.addChild(goal);
+    for(var i = 0; i < map.coords.objects.length; i++) {
+      var len = map.imgs.objects.length,
+          idx = parseInt(Math.random() * len, 10),
+          img_url = map.imgs.objects[idx]
+          obj = new createjs.Bitmap(img_url);
+      obj.x = map.coords.objects[i].x * 50;
+      obj.y = map.coords.objects[i].y * 50;
+      stage.addChild(obj);
+    }
+    stage.update();
+
+  });
+
+}
+
+function loadImg(img_url) {
+  var $img = $('<img style="display:none" src="' + img_url + '">'),
+      deferred = $.Deferred();
+  $img.load(function() {
+    deferred.resolve();
+  });
+  return deferred;
+}
+
+function extractMap(map) {
+  var coords = {};
+  coords[map.coords.character] = 1;
+  coords[map.coords.goal] = 1;
+  map.coords.character = getCoord(map.coords.character);
+  map.coords.goal = getCoord(map.coords.goal);
+  for(var i = 0; i < map.coords.roads.length; i++) {
+    coords[map.coords.roads[i]] = 1;
+    map.coords.roads[i] = getCoord(map.coords.roads[i]);
+  }
+  map.coords.objects = [];
+  for(var i = 0; i < 8; i++) {
+    for(var j = 0; j < 8; j++) {
+      if(!coords[j+","+i]) {
+        map.coords.objects.push({
+          x: j,
+          y: i
+        });
+      }
+    }
+  }
+}
+
+function getCoord(coord) {
+  return {
+    x: +coord.split(",")[0],
+    y: +coord.split(",")[1]
   }
 }
