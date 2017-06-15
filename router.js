911c85f7e20e3ae1c6ca221522f65df51bb66eb7
feelblu <feelblu@gmail.com>
2017-06-15 11:53:06 +0900

    울산지사 소속원 5세반에 6세 컨텐츠 추가

--- router.js
+++ router.js
@@ -237,6 +237,12 @@ function getBook(classObj) {
       return "B-" + (Number(bname.split("-")[1])-1);
     });
   }
+  // 울산지사 소속원 5세반에 6세 컨텐츠 추가
+  if(school.slice(0,6) == "A00072" && classObj.level == "A") {
+    book += "," + book.split(",").map(function(bname) {
+      return "B-" + (Number(bname.split("-")[1])-1);
+    });
+  }
   // 용인지사 성음유치원, 영업부 일산지사 홍익유치원 컨텐츠 추가
   if(classObj.code == "A00088-K1-KC1" || school == "C00149-K1") {
     book += ",추가컨텐츠";
