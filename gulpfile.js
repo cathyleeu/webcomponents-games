f677cd40a4e4dc31070e3e06dbbd9788ba8cfd4d
feelblu <feelblu@gmail.com>
2017-07-01 22:08:21 +0900

    browser-sync 제거

--- gulpfile.js
+++ gulpfile.js
@@ -15,40 +15,9 @@ gulp.task('less', function () {
     .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('server', ['less'], function() {
+gulp.task('default', ['less'], function() {
   var server = child.spawn('node', ['--harmony', 'app.js']);
   // var log = fs.createWriteStream('server.log', {flags: 'a'});
   // server.stdout.pipe(log);
   // server.stderr.pipe(log);
-});
-
-gulp.task('default', ['server'], function () {
-  var browserSync = require('browser-sync');
-  var reload = browserSync.reload;
-
-  browserSync({
-    port: 3000,
-    notify: false,
-    logPrefix: 'KC',
-    proxy: 'localhost:3000',
-    snippetOptions: {
-      rule: {
-        match: '<span id="browser-sync-binding"></span>',
-        fn: function (snippet) {
-          return snippet;
-        }
-      }
-    },
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
-    // server: {
-    //   baseDir: ['public']
-    // }
-  });
-
-  gulp.watch(['view/**/*.ejs'], [reload]);
-  gulp.watch(['public/css/**/*.less'], ['less', reload]);
-  gulp.watch(['public/{components,img,js,maze,sound}/**/*'], [reload]);
 });
