2b291cd865dbad713dc783f0fb426993d8f01bba
feelblu <feelblu@gmail.com>
2017-07-03 11:37:55 +0900

    폴리머 적용

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 'use strict';
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var fs = require('fs');
-var child = require('child_process');
-var glob = require('glob');
-var path = require('path');
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    PolymerProject = require('polymer-build').PolymerProject,
+    HtmlSplitter = require('polymer-build').HtmlSplitter,
+    mergeStream = require('merge-stream'),
+    fs = require('fs'),
+    child = require('child_process'),
+    glob = require('glob'),
+    path = require('path'),
+    polymerJson = require('./webcomponents/polymer.json'),
+    project = new PolymerProject(polymerJson),
+    sourcesHtmlSplitter = new HtmlSplitter();
 
 gulp.task('build', ['less']);
 
@@ -15,9 +21,23 @@ gulp.task('less', function () {
     .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('default', ['less'], function() {
-  var server = child.spawn('node', ['--harmony', 'app.js']);
-  // var log = fs.createWriteStream('server.log', {flags: 'a'});
-  // server.stdout.pipe(log);
-  // server.stderr.pipe(log);
+gulp.task('default', function() {
+  var server = child.spawn('node', ['index']);
+  server.stdout.on('data', function(data) {
+    process.stdout.write(data.toString());
+  });
+  server.stderr.on('data', function(data) {
+    process.stdout.write(data.toString());
+  });
+  $.watch('public/css/**/*.less', { ignoreInitial: false })
+    .pipe($.less())
+    .pipe(gulp.dest('public/css'));
+  $.watch('webcomponents/*.html', { ignoreInitial: false }, function () {
+    mergeStream(project.sources(), project.dependencies())
+      .pipe($.cleanDest('public'))
+      .pipe(sourcesHtmlSplitter.split())
+      .pipe(project.bundler())
+      .pipe(project.addCustomElementsEs5Adapter())
+      .pipe(gulp.dest('public'));
+  });
 });
