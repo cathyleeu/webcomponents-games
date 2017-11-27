148518e2ef84df188a3a51c26a4909e7800087a5
cathyleeu <leeyu823@gmail.com>
2017-11-27 15:01:02 +0900

    이미지 창작게임

--- public/webcomponents/activity-devtool.js
+++ public/webcomponents/activity-devtool.js
@@ -9,6 +9,10 @@ class actTool {
     d3.select(target).select(".bar_cont").style("display", "none")
     console.log("removeStatusBar");
   }
+  getCreateImageDots(){
+    let getEle = this.document.getElementById('element');
+    getEle.returnAxis()
+  }
   // getScreenShot() {
   //   let canvas = this.document.createElement("canvas"),
   //       ctx = canvas.getContext("2d")
