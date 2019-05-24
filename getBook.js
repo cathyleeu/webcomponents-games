87a1ab06921e22baa23ef5a9cf3cb0c63de21314
toycode <toycodeinc@gmail.com>
2019-05-24 09:00:55 +0000

    Update getBook.js

--- getBook.js
+++ getBook.js
@@ -1,7 +1,7 @@
 module.exports = function(classObj) {
   var schoolCode = classObj.code.split("-").slice(0, 2).join("-"),
       classCode = classObj.code,
-      book = [2, 3];
+      book = [3, 4];
 
   // 참사랑어린이집, 꽃내음어린이집 늑대와여우 태블릿(IE) 용량 문제로 본 진도 한권씩만
   if(schoolCode == "A00059-K1" || schoolCode == "A00059-K2") {
@@ -9,35 +9,35 @@ module.exports = function(classObj) {
   }
   // 인천 동심유치원 10월 6호 오픈 요청
   if(schoolCode == "C00194-K4") {
-    book = [1, 2];
+    book = [2, 3];
   }
   // 부산 키즈클럽 롯데어학원 19년2월까지 9권 요청
   if(schoolCode == "A00066-K1") {
-    book = [10.5, 1];
+    book = [1, 2];
   }
   // 초록나라 영재유치원 8월말 3호 오픈 요청
   if(schoolCode == "C00175-K4") {
-    book = [10, 10.5];
+    book = [10.5, 1];
   }
   // 초록나라 예원유치원 9월말 6호 오픈 요청
   if(schoolCode == "C00175-K1") {
-    book = [1, 2];
+    book = [2, 3];
   }
   // 안산지사 엠에스에듀 8월말 4호 오픈 요청
   if(schoolCode == "C00188-K2") {
-    book = [10.5, 1];
+    book = [1, 2];
   }
   // 양주 ECC 
   if(schoolCode == "B00171-K1") {
-    book = [3, 4];
+    book = [4, 5];
   }
   // 제천지사 창의놀이유치원 
   if(schoolCode == "C00204-K1") {
-    book = [1, 2];
+    book = [2, 3];
   }
   // 인천지사 참조은 유치원 7월에 1권 11월 중순에 3권 요청함
   if(schoolCode == "C00194-K1") {
-    book = [7, 8];
+    book = [8, 9];
   }
   // YBM영업부(내부용)
   if(schoolCode == "A00083-K3") {
