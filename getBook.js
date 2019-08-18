026788558015a4db635b80270976c3a05e1084eb
toycode <toycodeinc@gmail.com>
2019-08-18 23:27:43 +0000

    Update getBook.js

--- getBook.js
+++ getBook.js
@@ -7,6 +7,10 @@ module.exports = function(classObj) {
   if(schoolCode == "A00059-K1" || schoolCode == "A00059-K2") {
     book = [5.5];
   }
+  // 대구 햇살어린이집 방학권 미사용
+  if(schoolCode == "C00071-K13") {
+    book = [6];
+  }
   // 인천 동심유치원 10월 6호 오픈 요청
   if(schoolCode == "C00194-K4") {
     book = [4, 5];
