069c258e90687491ee413153e6341681f69e84d1
toycode <toycodeinc@gmail.com>
2019-08-30 09:20:57 +0000

    Update getBook.js

--- getBook.js
+++ getBook.js
@@ -5,7 +5,7 @@ module.exports = function(classObj) {
 
   // 참사랑어린이집, 꽃내음어린이집 늑대와여우 태블릿(IE) 용량 문제로 본 진도 한권씩만
   if(schoolCode == "A00059-K1" || schoolCode == "A00059-K2") {
-    book = [5.5];
+    book = [6];
   }
   // 대구 햇살어린이집, 하남SLP 방학권 미사용
   if(schoolCode == "C00071-K13" || schoolCode == "C00231-K2") {
