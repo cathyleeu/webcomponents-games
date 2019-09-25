872138cc7b72397f5d4cae90466ea59a0206a02a
toycode <toycodeinc@gmail.com>
2019-09-25 01:33:06 +0000

    Update getBook.js

--- getBook.js
+++ getBook.js
@@ -59,10 +59,6 @@ module.exports = function(classObj) {
   if(schoolCode == "C00192-K2") {
     book = [5.5, 7];
   }
-  // 명지국제ECC 19년 8월 4권 추가 요청
-  if(schoolCode == "B00031-K1") {
-    book = [4, 5, 5.5];  
-  }
   // 청라ECC, 광명ECC 2학기 신규 1권
   if(schoolCode == "B00016-K1" || schoolCode == "B00022-K1") {
     book = [1, 2, 6, 7];
