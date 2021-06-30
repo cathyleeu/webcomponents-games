8496bc371fc10972ac4107571354c7e9e4a55b90
toycode <toycodeinc@gmail.com>
2021-06-30 03:57:01 +0000

    Update getBook.js

--- getBook.js
+++ getBook.js
@@ -1,7 +1,7 @@
 module.exports = function(classObj) {
   var schoolCode = classObj.code.split("-").slice(0, 2).join("-"),
       classCode = classObj.code,
-      book = [5.5, 6];
+      book = [5, 5.5, 6];
 
    // 제주국제 ECC - 3/2적용
   if(schoolCode == "B00285-K1") {
